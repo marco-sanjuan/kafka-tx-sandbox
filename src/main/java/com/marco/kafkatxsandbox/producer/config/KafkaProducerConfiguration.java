0c3dbc56adc1e3333287a9eb55ed24aae51689f7
Marco San Juan <marco.sanjuan@gmail.com>
2019-08-21 13:57:21 +0200

    Reduce to minimum configuration needed

--- src/main/java/com/marco/kafkatxsandbox/producer/config/KafkaProducerConfiguration.java
+++ src/main/java/com/marco/kafkatxsandbox/producer/config/KafkaProducerConfiguration.java
@@ -18,9 +18,6 @@ public class KafkaProducerConfiguration {
 	@Value("${spring.kafka.bootstrap-servers}")
 	private String bootstrapServers;
 
-	@Value("${spring.kafka.producer.client-id}")
-	private String clientId;
-	
 	@Value("${spring.kafka.producer.transactionId-preffix}")
 	private String transactionIdPreffix;
 	
@@ -34,7 +31,6 @@ public class KafkaProducerConfiguration {
 	private Map<String,Object> producerConfigurations() {
 		final Map<String, Object> props = new HashMap<>();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-		props.put(ProducerConfig.CLIENT_ID_CONFIG, clientId);
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
 		return props;
